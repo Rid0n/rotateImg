fbb281989c75243a44bad26ccffa3b90eafe34bd
Rid0n <ridonms@gmail.com>
2021-01-20 02:29:48 +0300

    sorry util

--- util.c
+++ util.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-
+#include "image.h"
 _Noreturn void err( const char* msg, ... ) {
   va_list args;
   va_start (args, msg);
@@ -34,7 +34,7 @@ uint8_t* array_to_pixel_array(uint8_t** pixel_array,uint64_t width,uint64_t heig
     return array_pixel;
 }
 
-void print_double_pixel_array(struct pixel **array, uint32_t width,uint32_t height) {
+void print_double_pixel_array(struct pixel** array, uint32_t width,uint32_t height) {
     for (size_t i = 0; i < height; i++) {
         for (size_t j = 0; j < width; j++) {
             printf("Blue: %" PRIu8 " Green %" PRIu8 " Red %" PRIu8 "\n", array[i][j].b, array[i][j].g, array[i][j].r);
